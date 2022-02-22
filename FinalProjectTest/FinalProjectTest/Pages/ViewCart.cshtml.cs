e1453b24f7760cd3faa2c018f136679eeb89f216
codytran1994 <cody.tran@go.sfcollege.edu>
2022-02-21 21:20:58 -0500

    Added login and view cart pages
    Added pages for login and cart for proof of concept

---
+++ FinalProjectTest/FinalProjectTest/Pages/ViewCart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProjectTest.Pages
+{
+    public class ViewCartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
