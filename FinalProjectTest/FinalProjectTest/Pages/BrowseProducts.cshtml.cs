bd7863182ec4dd7a86f101d57ea8b2b8bc98a09a
KAMcCuller <kamcculler@gmail.com>
2022-02-23 18:28:43 -0500

    Added 'Browse Products' and 'Product Info' pages.

---
+++ FinalProjectTest/FinalProjectTest/Pages/BrowseProducts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProjectTest.Pages
+{
+    public class BrowseProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
