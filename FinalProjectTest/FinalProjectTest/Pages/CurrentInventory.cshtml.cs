498869dc12979959c97cc03eaa8bd92605684750
belac1997 <51928072+belac1997@users.noreply.github.com>
2022-02-23 19:55:22 -0500

    Add files via upload

---
+++ FinalProjectTest/FinalProjectTest/Pages/CurrentInventory.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProjectTest.Pages
+{
+    public class CurrentInventoryModel : PageModel
+    {
+        private readonly ILogger<CurrentInventoryModel> _logger;
+
+        public CurrentInventoryModel(ILogger<CurrentInventoryModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ FinalProjectTest/FinalProjectTest/Pages/InventoryEdit.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProjectTest.Pages
+{
+    public class InventoryEditModel : PageModel
+    {
+        private readonly ILogger<InventoryEditModel> _logger;
+
+        public InventoryEditModel(ILogger<InventoryEditModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
